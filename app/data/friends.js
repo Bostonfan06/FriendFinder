e1536b6b39108cdec2b4b6ac23d3daff037115de
Sean McPheeters <alexissean@Seans-MacBook-Pro.local>
2018-04-21 15:55:18 -0700

    Updated Files

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friendsArray = [{
+    name: "Johnny Depp",
+    sex: "male",
+    photo: "./images/johnnydepp.jpg",
+    scores: [
+        1,
+        5,
+        3,
+        3,
+        4
+    ]
+},
+{
+    name: "Cameron Diaz",
+    photo: "./images/camerondiaz.jpg",
+    scores: [
+        3,
+        5,
+        4,
+        4,
+        3
+    ]
+},
+{
+    name: "Brad Pitt",
+    photo: "./images/bradpitt.jpg",
+    scores: [
+        5,
+        3,
+        3,
+        2,
+        5
+    ]
+},
+{
+    name: "Paris Hilton",
+    photo: "./images/parishilton.jpg",
+    scores: [
+        2,
+        1,
+        2,
+        4,
+        5
+    ]
+},
+{
+    name: "Megan Fox",
+    photo: "./images/meganfox.jpg",
+    scores: [
+        3,
+        4,
+        2,
+        5,
+        4
+    ]
+},
+{
+    name: "Dwayne Johnson",
+    photo: "./images/dwaynejohnson.jpg",
+    scores:[
+        5,
+        4,
+        3,
+        2,
+        2
+    ]
+}]
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var friendsTable = require('../data/friends.js');
+
+module.export = function(app){
+
+    app.get('/api/friends', function(req, res){
+        result.JSON(friendsTable);
+    });
+
+    app.post("/api/friends", function(req, res){
+        var you = req.body;
+        var newFriend = -1;
+        var newFriendScore = 100;
+        var currentFriendScore = 0;
+
+        for (i = 0; i < friendsTable.length; i++){
+
+            if (you.sex != friendsTable[i].sex){
+                for (j = 0; j < you.scores.length; j++){
+                    currentFriendScore = currentFriendScore + Math.abs(friendsTable[i].scores[j] - you.scores[j]);
+                }
+
+                if (currentFriendScore <= newFriendScore){
+                    newFriend = i;
+                    newFriendScore = currentFriendScore;
+                }
+
+                currentFriendScore = 0;
+            }
+        }
+
+        friendsTable.push(you);
+        newFriendDetails = friendsTable[newFriend];
+        results.JSON(newFriendDetails);
+    })
+};
