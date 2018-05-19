7b161df6abeb42b3d3566cde29263d3905ddd31e
Sean McPheeters <alexissean@dhcp-v121-019.mobile.uci.edu>
2018-05-19 09:31:09 -0700

    Updated Files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 var app  = express(); 
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/public"))
 
